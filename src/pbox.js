1f10739f18165dbdeb4ffbebac785c9345542f87
xuhaifeng <haifeng@worktile.com>
2013-08-18 22:50:22 +0800

    change pbox tp support align left right

--- src/pbox.js
+++ src/pbox.js
@@ -13,16 +13,17 @@
     "use strict";
 
     var defaults = {
-        templateUrl    : null,
-        template       : "",
-        placement      : "bottom",
-        animation      : true,
-        popupDelay     : 0,
-        triggerClass   : 'in',
-        popboxOpenClass: 'pbox-open',
-        autoClose      : true,
-        offset         : 4,
-        align          : null
+        templateUrl : null,
+        template    : "",
+        placement   : "bottom",
+        animation   : true,
+        popupDelay  : 0,
+        triggerClass: 'in',
+        openClass   : 'pbox-open',
+        closeClass  : 'pbox-close',
+        autoClose   : true,
+        offset      : 4,
+        align       : null
     }
 
     var tools = {
@@ -31,15 +32,24 @@
 
     $.fn.extend({
         pBox: function (options) {
-            var options = $.extend({}, defaults, options);
             var $document = $(document), $body = $(document.body);
 
             function pBoxFn($element, options) {
                 var self = this;
-                this.options = options;
+                this.options = $.extend({}, defaults, options);
+                ;
                 this.$element = $element;
                 this.boxElement = null;
                 var init = function () {
+                    var align = self.$element.attr("data-align") || self.$element.attr("align");
+                    if (align !== undefined && align !== null) {
+                        self.options.align = align;
+                    }
+                    var placement = self.$element.attr("data-placement") || self.$element.attr("data-placement");
+                    if (placement !== undefined && placement !== null) {
+                        self.options.placement = placement;
+                    }
+
                     var template = "";
                     if (!$.isEmptyObject(options.template)) {
                         template = options.template;
@@ -58,6 +68,10 @@
                     $element.bind("click.pbox", function () {
                         self.open();
                     })
+                    boxElement.find(".close").bind("click",function(){
+                        self.close();
+                    })
+
                 }
 
                 init();
@@ -67,13 +81,15 @@
 
             pBoxFn.prototype.open = function () {
                 var pBox = this;
-                if (this.$element.hasClass("open")) {
+                if (this.$element.hasClass(this.options.openClass)) {
                     this.close();
                 } else {
                     var elementTop = this.$element.offset().top,
                         elementLeft = this.$element.offset().left,
                         elementRight = this.$element.offset().right,
-                        top , left,
+                        dicOuterWidth = $document.outerWidth(),
+                        dicOuterHeight = $document.outerHeight(),
+                        top , left, right, bottom,
                         elementOuterWidth = this.$element.outerWidth(),
                         elementOuterHeight = this.$element.outerHeight(),
                         boxWidth = pBox.boxElement.outerWidth(true),
@@ -85,30 +101,49 @@
                     switch (this.options.placement) {
                         case "bottom":
                             top = elementTop + elementOuterHeight + this.options.offset;
-                            if (options.align === "left") {
+                            if (pBox.options.align === "left") {
                                 left = elementLeft;
-                            } else if (options.align === "right") {
-                                left = elementLeft;
+                            } else if (pBox.options.align === "right") {
+                                left = elementLeft + elementOuterWidth - boxWidth;
+                                //right = elementRight - elementOuterWidth;
                             }
                             else {
                                 left = elementLeft - boxWidth / 2 + elementOuterWidth / 2;
                             }
-                            if(top > 0)
-                            this.boxElement.css("top", top);
-                            this.boxElement.css("left", left);
                             break;
-                        case
-                        "top"
-                        :
+                        case "top":
                             break;
                         default:
                             break;
                     }
 
-                    this.boxElement.addClass("open");
-                    this.boxElement.removeClass("close");
-                    this.$element.addClass("open");
-                    //this.$element.removeClass("close");
+                    if (top !== undefined) {
+                        if(top + boxHeight > dicOuterHeight){
+                            //this.boxElement.css("bottom", top);
+                            this.boxElement.css("top", elementTop - boxHeight - pBox.options.offset);
+                        }else{
+                            this.boxElement.css("top", top);
+                        }
+
+                    }
+                    if (left !== undefined) {
+                        if(left < 0){
+                            left = 4;
+                        }
+                        if(left + boxWidth > dicOuterWidth){
+                            this.boxElement.css("right", 4);
+                        }else{
+                            this.boxElement.css("left", left);
+                        }
+
+                    }
+                    if (right !== undefined) {
+                        this.boxElement.css("right", right);
+                    }
+
+                    this.boxElement.addClass(this.options.openClass);
+                    this.boxElement.removeClass(this.options.closeClass);
+                    this.$element.addClass(this.options.openClass);
 
                     if (this.options.autoClose === true) {
                         this.$element.bind("mousedown.pbox", function (event) {
@@ -127,10 +162,9 @@
             }
 
             pBoxFn.prototype.close = function () {
-                this.$element.removeClass("open");
-                //this.$element.addClass("close");
-                this.boxElement.removeClass("open");
-                this.boxElement.addClass("close");
+                this.$element.removeClass(this.options.openClass);
+                this.boxElement.removeClass(this.options.openClass);
+                this.boxElement.addClass(this.options.closeClass);
 
                 if (this.options.autoClose === true) {
                     this.$element.unbind("mousedown.pbox");
